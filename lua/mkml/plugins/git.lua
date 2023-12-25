94794ea270d1a6b55acd53b95d3902043d2d5909
mkmlman <naveen.adhulu@gmail.com>
2023-12-25 14:25:52 +0100

    add vim-fugitive plugin

--- lua/mkml/plugins/git.lua
+++ lua/mkml/plugins/git.lua
@@ -1,8 +1,9 @@
 return {
-  'dinhhuy258/git.nvim',
-  -- this is currently not needed, as i can live with using :!git instead of :Git for now. once i'm fuming with no color highlights, then can enable this. 
-  -- this plugin is a wrapper on vim-fugitive for neovim. 
---  config = function()
-  --  require('git').setup()
-    --end
+    "tpope/vim-fugitive",
+    lazy = false,
+    keys = {
+      { "<leader>gb", "<cmd>G blame<cr>", desc = "read file from git" },
+      { "<leader>gs", "<cmd>G status<cr>", desc = "git status from git" }, 
+    },
+    dependencies = { "tpope/vim-rhubarb" },
 }
