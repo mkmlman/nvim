172d1405c3a2424f99888afa762f02542e69f629
mkmlman <naveen.adhulu@gmail.com>
2023-12-25 13:58:03 +0100

    disable gitwq plugin

--- lua/mkml/plugins/git.lua
+++ lua/mkml/plugins/git.lua
@@ -1,6 +1,8 @@
 return {
   'dinhhuy258/git.nvim',
-  config = function()
-    require('git').setup()
-    end
+  -- this is currently not needed, as i can live with using :!git instead of :Git for now. once i'm fuming with no color highlights, then can enable this. 
+  -- this plugin is a wrapper on vim-fugitive for neovim. 
+--  config = function()
+  --  require('git').setup()
+    --end
 }
